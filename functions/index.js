d62fde0957c82269a550c8d53a70de4549493b8b
Joe Whelton <joe.whelton@mycit.ie>
2020-10-15 14:08:22 +0100

    Finished basic CRUD operations

--- functions/index.js
+++ functions/index.js
@@ -11,8 +11,15 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const {
-  getAllTodos
+    getAllTodos,
+    postOneTodo,
+    deleteTodo,
+    editTodo
 } = require('./APIs/todos')
 
 app.get('/todos', getAllTodos);
+app.post('/todos', postOneTodo);
+app.delete('/todo/:todoId', deleteTodo);
+app.put('/todo/:todoId', editTodo);
+
 exports.api = functions.https.onRequest(app);
